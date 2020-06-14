6555c1e65d9f49173fe59688ee52ad5e474cbd4b
wanghong <34284143@qq.com>
2020-06-14 23:28:14 +0800

    合并开发分支成功"

---
+++ config/webpack.prod.conf.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        chunkFilename: '[name].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '/'
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./dist",
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            { test: /\.(png|svg|jpg|gif)$/, use: ["file-loader"] },
+        ],
+    },
+};
